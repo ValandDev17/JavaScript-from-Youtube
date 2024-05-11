da60a4bba3efb2b93ec882b5a384203006bd2e90
DevValand <devvaland17@gmail.com>
2024-05-11 12:45:00 +0530

    changes

--- 4_Iteration/4 Filter map & reduce.js
+++ 4_Iteration/4 Filter map & reduce.js
@@ -6,7 +6,6 @@
 // console.log(values);
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
 //if we enclose with {} then must write explicit return
